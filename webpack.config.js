7b466b744bdd9a6415fa74de625e62d44afa3fe3
Dan Rice <dan.rice@sparkmeter.io>
2022-05-10 22:31:25 -0400

    Copy content from nerves_hub_www
    * Splice templates together to produce a single HTML page
    * Organize assets in the root and reference using relative paths
    * Process (copy) index.html during webpack build
    * Remove Phoenix

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const glob = require('glob')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = (env, options) => ({
+  cache: true,
+  optimization: {
+    minimizer: [
+      new TerserPlugin({ parallel: true }),
+      new CssMinimizerPlugin({}),
+    ],
+  },
+  entry: {
+    app: glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '_dist/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-url'],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '../images',
+        },
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff2?)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '../fonts',
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '..',
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      __dirname + '/js',
+      __dirname + '/css',
+      __dirname + '/images',
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new CopyWebpackPlugin({ patterns: [
+      { from: 'static/', to: '../' },
+      { from: '*.html', to: '../' },
+    ] }),
+  ],
+})
