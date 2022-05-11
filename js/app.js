6bdf469f1f24e1af46211b2d619367553ef2ce82
Dan Rice <dan.rice@sparkmeter.io>
2022-05-10 22:31:26 -0400

    Remove unused JS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
 import '../css/app.scss'
 
-import 'bootstrap'
-import '@popperjs/core'
-import $ from 'jquery'
 import Josh from 'joshjs'
 
 new Josh()
-
-$(function () {
-  $('.custom-upload-input').on('change', function () {
-    let fileName = $(this).val().split('\\').pop()
-    $(this)
-      .siblings('.custom-upload-label')
-      .removeClass('not-selected')
-      .addClass('selected')
-      .html("Selected File: <div class='file-name'>" + fileName + '</div>')
-  })
-})
